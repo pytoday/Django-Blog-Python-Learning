8e5c8a6caaec0e819fde9329ea4df561ebaa79e3
pytoday <jackietsui72@gmail.com>
2018-11-12 20:39:58 +0800

    change sec key

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'f*6++g&bl=fxlub!2az)+tst+z@wzt2ilrukpu9@&)a5lo7d%8'
+SECRET_KEY = 'some sec key here'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
